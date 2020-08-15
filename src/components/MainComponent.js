8086dd746183a8932cb6535d6a7d9fdc3b65e8ed
alhec <hector080896@gmail.com>
2020-08-14 20:35:58 -0400

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -44,11 +45,17 @@ class Main extends Component {
       );
     };
 
+    const AboutPage= () => {
+      return (
+        <About leaders={this.state.leaders}/>
+      );
+    }
     return (
         <div>
         <Header/>
         <Switch>
             <Route path='/home' component={HomePage} />
+            <Route exact path='/aboutus' component={AboutPage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />
